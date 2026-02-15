9587901ba214058bb465c5844db652d7e893be93
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-02-15 21:48:32 +0600

    change wezterm config theme

--- wezterm.lua
+++ wezterm.lua
@@ -1,20 +1,20 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- --- Appearance ---
-config.color_scheme = 'Tokyo Night' -- One of 1000+ built-ins
-config.font = wezterm.font('JetBrains Mono Nerd Font')
+config.color_scheme = "Gruvbox dark, hard (base16)" -- One of 1000+ built-ins
+config.font = wezterm.font("JetBrains Mono Nerd Font")
 config.font_size = 13.0
 config.window_background_opacity = 1
 config.macos_window_background_blur = 10 -- macOS only
-config.window_decorations = "RESIZE"     -- Cleaner title bar
+config.window_decorations = "RESIZE" -- Cleaner title bar
 
 -- remove side padding
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 -- --- Tab Bar ---
