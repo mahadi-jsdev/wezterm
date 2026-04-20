1d24dedb4fc96e4bf17c12b6de1cfd4e54df9293
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-04-20 19:02:57 +0600

    change theme to catpuccin

--- wezterm.lua
+++ wezterm.lua
@@ -2,24 +2,24 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- --- Appearance ---
-config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrains Mono Nerd Font")
 config.font_size = 13.0
 config.window_background_opacity = 1
 config.macos_window_background_blur = 15
 config.window_decorations = "RESIZE"
 config.inactive_pane_hsb = {
-	hue = 1.0,
-	saturation = 0.9,
-	brightness = 0.8,
+  hue = 1.0,
+  saturation = 0.9,
+  brightness = 0.8,
 }
 
 -- remove side padding
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 --- Tab Bar ---
@@ -27,12 +27,12 @@ config.enable_tab_bar = false
 
 -- keymaps
 config.keys = {
-	-- Show the Launcher (Tab/Pane picker)
-	{
-		key = "l",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.ShowLauncherArgs({ flags = "TABS" }),
-	},
+  -- Show the Launcher (Tab/Pane picker)
+  {
+    key = "l",
+    mods = "SHIFT|CTRL",
+    action = wezterm.action.ShowLauncherArgs({ flags = "TABS" }),
+  },
 }
 
 -- --- Performance ---
